5090ff9c0afbe78706f6b9e84eae1076171fdb5a
Mathabib <91876177+Mathabib@users.noreply.github.com>
2022-07-07 18:44:13 +0700

    Add files via upload

---
+++ koreksikuissmtk_deserialisasi/src/main/java/serialisasi/deserialisasi/jawaban.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package serialisasi.deserialisasi;
+import java.io.*;
+/**
+ *
+ * @author Ahmad Habib
+ */
+public class jawaban implements Serializable{
+String jawaban1;
+String jawaban2;
+String jawaban3;
+String jawaban4;
+
+public jawaban(String a, String b, String c, String d){
+this.jawaban1 = a;
+this.jawaban2 = b;
+this.jawaban3 = c;
+this.jawaban4 = d;
+
+}
+    
+}
